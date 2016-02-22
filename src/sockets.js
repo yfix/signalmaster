b2faeee822ccbc7073aae122fbd8b16b725bcf27
Sergey Mushta <futukrishna@gmail.com>
2016-02-22 12:30:37 +0200

    fix

--- src/sockets.js
+++ src/sockets.js
@@ -14,49 +14,49 @@ module.exports = function (server, config, mysql) {
     io.sockets.on('connection', function (socket) {
         
         socket.on('login', function (name) {
-          // if this socket is already connected,
-          // send a failed login message
-          if (_.findIndex(users, { socket: socket.id }) !== -1) {
-            socket.emit('login_error', 'You are already connected.');
-          }
+            // if this socket is already connected,
+            // send a failed login message
+            if (_.findIndex(users, { socket: socket.id }) !== -1) {
+                socket.emit('login_error', 'You are already connected.');
+            }
 
-          // if this name is already registered,
-          // send a failed login message
-          if (_.findIndex(users, { name: name }) !== -1) {
-            socket.emit('login_error', 'This name already exists.');
-            return; 
-          }
+            // if this name is already registered,
+            // send a failed login message
+            if (_.findIndex(users, { name: name }) !== -1) {
+                socket.emit('login_error', 'This name already exists.');
+                return; 
+            }
 
-          users.push({ 
-            name: name,
-            socket: socket.id
-          });
+            users.push({ 
+                name: name,
+                socket: socket.id
+            });
 
-          socket.emit('login_successful', _.map(users, 'name'));
-          socket.broadcast.emit('online', name);
+            socket.emit('login_successful', _.map(users, 'name'));
+            socket.broadcast.emit('online', name);
 
-          console.log(name + ' logged in');
+            console.log(name + ' logged in');
         });
 
         socket.on('sendMessage', function (name, message) {
-          var currentUser = _.find(users, { socket: socket.id });
-          if (!currentUser) { return; }
+            var currentUser = _.find(users, { socket: socket.id });
+            if (!currentUser) { return; }
 
-          var contact = _.find(users, { name: name });
-          if (!contact) { return; }
+            var contact = _.find(users, { name: name });
+            if (!contact) { return; }
 
-          io.to(contact.socket)
-            .emit('messageReceived', currentUser.name, message);
+            io.to(contact.socket)
+                .emit('messageReceived', currentUser.name, message);
         });
 
         socket.on('disconnect', function () {
-          var index = _.findIndex(users, { socket: socket.id });
-          if (index !== -1) {
-            socket.broadcast.emit('offline', users[index].name);
-            console.log(users[index].name + ' disconnected');
+            var index = _.findIndex(users, { socket: socket.id });
+            if (index !== -1) {
+                socket.broadcast.emit('offline', users[index].name);
+                console.log(users[index].name + ' disconnected');
 
-            users.splice(index, 1);
-          }
+                users.splice(index, 1);
+            }
         });
 /*
         client.resources = {
